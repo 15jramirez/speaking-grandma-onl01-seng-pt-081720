7835cee1a37f6400509db54b597499a33051232b
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-20 05:47:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase !=="".downcase
-    return "NO< NOT SINCE 1938!"
+  elseif phrase !=="hello".downcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
